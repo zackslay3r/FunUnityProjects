4055fe062a60ac7f395448bd3ac92255e2396fba
Zackary D <zackary.direen@hotmail.com>
2018-11-21 17:03:44 +1100

    spawner/hit effects.

---
+++ FunProjects/Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+    Rigidbody m_Rigidbody;
+    // Use this for initialization
+    void Start () {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        m_Rigidbody.velocity = -transform.right * speed;
+	}
+}
